63e611342202cfbf7afc6e382ab99884644b16ba
nikhilc52 <147773549+nikhilc52@users.noreply.github.com>
2024-08-12 19:45:06 -0400

    fixed loading for videos

--- video.js
+++ video.js
@@ -1,13 +1,19 @@
-var video = document.getElementById("butterfly-landing");
-video.addEventListener("canplay", function () {
+var videolanding = document.getElementById("butterfly-landing");
+
+videolanding.addEventListener("canplay", function () {
     setTimeout(function () {
-        video.play();
+        videolanding.play();
     }, 7000);
 });
 
+videolanding.addEventListener("ended", function () {
+    videolanding.setAttribute('src', "images/butterfly-loop.mp4");
+    videolanding.load();
+    videolanding.play();
+});
 
-video.addEventListener("ended", function () {
-    video.setAttribute('src', "images/butterfly-loop.mp4");
-    video.load();
-    video.play();
+var videoacross = document.getElementById("butterfly");
+videoacross.addEventListener("canplay", function () {
+    videoacross.play();
+    videoacross.classList.add("butterfly-animation");
 });
