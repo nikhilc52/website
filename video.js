73ca71212817be4b336061e11bc6a9e2a9bd98af
nikhilc52 <147773549+nikhilc52@users.noreply.github.com>
2024-08-13 17:43:53 -0400

    added still image as default and comments

--- video.js
+++ video.js
@@ -1,6 +1,7 @@
 var videoacross = document.getElementById("butterfly");
 var videolanding = document.getElementById("butterfly-landing");
 
+//once the across video has ended, wait .5s then play the landing video and add the video animation
 videoacross.addEventListener("ended", function () {
     setTimeout(function () {
         videolanding.play();
@@ -8,11 +9,15 @@ videoacross.addEventListener("ended", function () {
     videolanding.classList.add("butterfly-landing-animation");
 });
 
+//once the landing video has ended, switch to the looping video and make the poster a still for time inbetween loops
 videolanding.addEventListener("ended", function () {
     videolanding.setAttribute('src', "images/butterfly-loop.mp4");
+    videolanding.setAttribute('poster', "images/butterfly-loop-still.png");
     videolanding.play();
 });
 
+//once you can play the across video, play it with the animation 
+//(needed so that slow loading sites don't have the animation start before the video)
 videoacross.addEventListener("canplay", function () {
     videoacross.play();
     videoacross.classList.add("butterfly-animation");
